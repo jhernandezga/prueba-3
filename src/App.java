892eb37b44336460ee4222a7c14b4bbb75097265
Jorge Andrés Hernández Galeano <57245076+jhernandezga@users.noreply.github.com>
2020-07-09 12:48:24 -0500

    aa

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo");
-        System.out.println("hola");
-		System.out.println("mundo 2");
+        
     }
 }
